f89f235ebaa101e7870a3fe143569ded30b9247e
Chien Wei Chou <chien.w.chou@gmail.com>
2025-06-22 11:32:12 -0700

    add credit card icons

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
+import americanExpressIcon from "@/assets/credit-icons/american-express.png";
+import chinaUnionPayIcon from "@/assets/credit-icons/china-union-pay.jpg";
+import dinersIcon from "@/assets/credit-icons/diners.png";
+import discoverIcon from "@/assets/credit-icons/discover.png";
+import jcbIcon from "@/assets/credit-icons/jcb.png";
+import mastercardIcon from "@/assets/credit-icons/mastercard.png";
+import paypalIcon from "@/assets/credit-icons/paypal.png";
+import visaIcon from "@/assets/credit-icons/visa.png";
 
 const Footer: React.FC = () => {
   return (
@@ -15,7 +23,10 @@ const Footer: React.FC = () => {
             <div className="mb-6">
               <h3 className="text-xl font-semibold mb-2">Need Help?</h3>
               <p className="mb-1">
-                Visit our <Link href="/support" className="underline">Customer Support</Link>
+                Visit our{" "}
+                <Link href="/support" className="underline">
+                  Customer Support
+                </Link>
               </p>
               <p className="mb-4">for assistance or call us at</p>
               <p className="text-xl font-bold">123-456-7890</p>
@@ -40,12 +51,24 @@ const Footer: React.FC = () => {
           <div>
             <h3 className="text-xl font-semibold mb-4">Menu</h3>
             <ul className="space-y-2">
-              <li><Link href="/deals">Deals</Link></li>
-              <li><Link href="/food">Food</Link></li>
-              <li><Link href="/beverages">Beverages</Link></li>
-              <li><Link href="/household">Household</Link></li>
-              <li><Link href="/personal-care">Personal Care</Link></li>
-              <li><Link href="/my-orders">My Orders</Link></li>
+              <li>
+                <Link href="/deals">Deals</Link>
+              </li>
+              <li>
+                <Link href="/food">Food</Link>
+              </li>
+              <li>
+                <Link href="/beverages">Beverages</Link>
+              </li>
+              <li>
+                <Link href="/household">Household</Link>
+              </li>
+              <li>
+                <Link href="/personal-care">Personal Care</Link>
+              </li>
+              <li>
+                <Link href="/my-orders">My Orders</Link>
+              </li>
             </ul>
           </div>
 
@@ -53,14 +76,30 @@ const Footer: React.FC = () => {
           <div>
             <h3 className="text-xl font-semibold mb-4">Categories</h3>
             <ul className="space-y-2">
-              <li><Link href="/vegetables">Vegetables</Link></li>
-              <li><Link href="/bakery">Bakery</Link></li>
-              <li><Link href="/wine">Wine</Link></li>
-              <li><Link href="/dairy-eggs">Dairy & Eggs</Link></li>
-              <li><Link href="/meat-poultry">Meat & Poultry</Link></li>
-              <li><Link href="/soft-drinks">Soft Drinks</Link></li>
-              <li><Link href="/cleaning-supplies">Cleaning Supplies</Link></li>
-              <li><Link href="/cereal-snacks">Cereal & Snacks</Link></li>
+              <li>
+                <Link href="/vegetables">Vegetables</Link>
+              </li>
+              <li>
+                <Link href="/bakery">Bakery</Link>
+              </li>
+              <li>
+                <Link href="/wine">Wine</Link>
+              </li>
+              <li>
+                <Link href="/dairy-eggs">Dairy & Eggs</Link>
+              </li>
+              <li>
+                <Link href="/meat-poultry">Meat & Poultry</Link>
+              </li>
+              <li>
+                <Link href="/soft-drinks">Soft Drinks</Link>
+              </li>
+              <li>
+                <Link href="/cleaning-supplies">Cleaning Supplies</Link>
+              </li>
+              <li>
+                <Link href="/cereal-snacks">Cereal & Snacks</Link>
+              </li>
             </ul>
           </div>
 
@@ -69,10 +108,18 @@ const Footer: React.FC = () => {
             <div>
               <h3 className="text-xl font-semibold mb-4">Info</h3>
               <ul className="space-y-2">
-                <li><Link href="/faq">FAQ</Link></li>
-                <li><Link href="/about">About Us</Link></li>
-                <li><Link href="/support">Customer Support</Link></li>
-                <li><Link href="/locations">Locations</Link></li>
+                <li>
+                  <Link href="/faq">FAQ</Link>
+                </li>
+                <li>
+                  <Link href="/about">About Us</Link>
+                </li>
+                <li>
+                  <Link href="/support">Customer Support</Link>
+                </li>
+                <li>
+                  <Link href="/locations">Locations</Link>
+                </li>
               </ul>
             </div>
 
@@ -80,8 +127,12 @@ const Footer: React.FC = () => {
             <div>
               <h3 className="text-xl font-semibold mb-4">My Choice</h3>
               <ul className="space-y-2">
-                <li><Link href="/favorites">Favorites</Link></li>
-                <li><Link href="/my-orders">My Orders</Link></li>
+                <li>
+                  <Link href="/favorites">Favorites</Link>
+                </li>
+                <li>
+                  <Link href="/my-orders">My Orders</Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -93,40 +144,96 @@ const Footer: React.FC = () => {
         {/* Footer Bottom */}
         <div className="text-center">
           {/* Policy Links */}
-          <div className="flex justify-center space-x-6 mb-8">
-            <Link href="/shipping-returns" className="underline">Shipping & Returns</Link>
-            <Link href="/terms" className="underline">Terms & Conditions</Link>
-            <Link href="/payment" className="underline">Payment Methods</Link>
+          <div className="flex justify-center space-x-9 mb-8">
+            <Link href="/shipping-returns" className="underline">
+              Shipping & Returns
+            </Link>
+            <Link href="/terms" className="underline">
+              Terms & Conditions
+            </Link>
+            <Link href="/payment" className="underline">
+              Payment Methods
+            </Link>
           </div>
 
           {/* Payment Methods */}
           <div>
-            <h3 className="text-xl mb-6">We accept the following payment methods</h3>
-            <div className="flex justify-center flex-wrap gap-4">
+            <h3 className="text-xl mb-6">
+              We accept the following payment methods
+            </h3>
+            <div className="flex justify-center flex-wrap gap-8">
               {/* Payment Icons */}
               <div className="bg-white p-2 rounded">
-                <Image src="/mastercard.png" alt="Mastercard" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={mastercardIcon}
+                  alt="Mastercard"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/amex.png" alt="American Express" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={americanExpressIcon}
+                  alt="American Express"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/unionpay.png" alt="UnionPay" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={chinaUnionPayIcon}
+                  alt="UnionPay"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/jcb.png" alt="JCB" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={jcbIcon}
+                  alt="JCB"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/discover.png" alt="Discover" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={americanExpressIcon}
+                  alt="Discover"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/visa.png" alt="Visa" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={visaIcon}
+                  alt="Visa"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/paypal.png" alt="PayPal" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={paypalIcon}
+                  alt="PayPal"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
               <div className="bg-white p-2 rounded">
-                <Image src="/diners.png" alt="Diners Club" width={40} height={24} className="h-6 w-auto" />
+                <Image
+                  src={dinersIcon}
+                  alt="Diners Club"
+                  width={40}
+                  height={24}
+                  className="h-6 w-auto"
+                />
               </div>
             </div>
           </div>
