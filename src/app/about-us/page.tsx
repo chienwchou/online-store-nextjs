addab27efcd3b42a655a284552f7221598275ed9
Chien Wei Chou <chien.w.chou@gmail.com>
2025-06-22 16:15:23 -0700

    add about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,16 @@
+import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header";
 
 export default function AboutUs() {
   return (
     <>
       <Header />
+      <section>
+        <div className="px-10 py-20">
+          <h1 className="text-7xl font-bold text-center">About Us</h1>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 }
