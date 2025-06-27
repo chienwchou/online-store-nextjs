fedd628b69465915e4abb57b7d653c5446a58d37
Chien Wei Chou <chien.w.chou@gmail.com>
2025-06-26 22:13:45 -0700

    modify more form styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header";
 import { useApp } from "@/contexts/AppContext";
+import Link from "next/link";
 
 export default function UserProfile() {
   const { user, theme, setUser, setTheme, logout } = useApp();
@@ -20,7 +21,7 @@ export default function UserProfile() {
         >
           <div className="flex flex-col w-full">
             <h1 className="text-4xl mb-8">Market Sign In</h1>
-            <form className="gap-y-6 flex flex-col w-full">
+            <form className="gap-y-6 flex flex-col w-full mb-2">
               <h2 className="font-semibold text-3xl">
                 Sign in for existing users
               </h2>
@@ -56,6 +57,10 @@ export default function UserProfile() {
                 LOGIN
               </button>
             </form>
+            <hr className="my-2 text-gray-400" />
+            <Link href="/forgot-password" className="text-blue-500">
+              Forgot your password?
+            </Link>
           </div>
         </div>
       </div>
