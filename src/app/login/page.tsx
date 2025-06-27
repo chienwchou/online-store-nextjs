1e5e4b1229a3ae1a967fa0716380904798d04ed4
Chien Wei Chou <chien.w.chou@gmail.com>
2025-06-26 21:45:19 -0700

    adjust login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 import { useApp } from "@/contexts/AppContext";
 
 export default function UserProfile() {
@@ -9,44 +11,49 @@ export default function UserProfile() {
     setUser({ name: "John Doe", email: "john@example.com" });
   };
 
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   return (
-    <div
-      className={`p-4 ${
-        theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"
-      }`}
-    >
-      <h2>User Profile</h2>
-
-      {user ? (
-        <div>
-          <p>Welcome, {user.name}!</p>
-          <p>Email: {user.email}</p>
-          <button
-            onClick={logout}
-            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
-          >
-            Logout
-          </button>
-        </div>
-      ) : (
-        <button
-          onClick={handleLogin}
-          className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-        >
-          Login
-        </button>
-      )}
-
-      <button
-        onClick={toggleTheme}
-        className="bg-gray-500 text-white px-4 py-2 rounded"
+    <>
+      <Header />
+      <div
+        className={`p-4 md:min-h-[1000px] flex flex-col items-center justify-center`}
       >
-        Toggle Theme ({theme})
-      </button>
-    </div>
+        <div className="max-w-7xl flex flex-col">
+          <h1 className="text-4xl mb-8">Market Sign In</h1>
+          <form>
+            <div className="form-control flex flex-col">
+              <label htmlFor="email" className="font-semibold">
+                Email Address
+              </label>
+              <input
+                id="email"
+                type="text"
+                name="email"
+                className="h-10 w-full"
+              />
+            </div>
+          </form>
+          {user ? (
+            <div>
+              <p>Welcome, {user.name}!</p>
+              <p>Email: {user.email}</p>
+              <button
+                onClick={logout}
+                className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={handleLogin}
+              className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
